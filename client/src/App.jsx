523131f8ad846f87ef15b346204fa30b823dfa23
Deb Johnson <friendswithdrj@gmail.com>
2020-12-28 20:47:53 -0600

    Add componentDidMount/words array to state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import HangmanPic from './components/HangmanPic.jsx';
 
 const hangmanpics = ["https://hangman-pics.s3.us-east-2.amazonaws.com/empty.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/first_wrong_guess.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/second_wrong_guess.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/third_wrong_guess.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/fourth_wrong_guess.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/fifth_wrong_guess.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/sixth_wrong_guess.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/seventh_wrong_guess.jpg", "https://hangman-pics.s3.us-east-2.amazonaws.com/eighth_wrong_guess.jpg"];
@@ -7,11 +8,22 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPic: hangmanpics[0]
+      currentPic: hangmanpics[0],
+      words: []
     }
   }
 
-
+  componentDidMount() {
+    axios.get('/api/words/all')
+      .then(res => {
+        this.setState({
+          words: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
 
   render () {
     return (
