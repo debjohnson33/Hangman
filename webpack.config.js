b4ab482bcd606c6374bc35e7d54f957fa9990afa
Deb Johnson <friendswithdrj@gmail.com>
2020-12-28 18:31:10 -0600

    Change location of bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: path.join(__dirname, '/client/src/index.jsx'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public/dist')
   },
   watch: true,
   module: {
