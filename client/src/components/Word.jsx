97ec3dcf993b7b7c393d9b93b901b0f0d60cf1db
Deb Johnson <friendswithdrj@gmail.com>
2020-12-30 18:11:20 -0600

    Add useState hook/import LetterGuessForm

--- client/src/components/Word.jsx
+++ client/src/components/Word.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LetterGuessForm from './LetterGuessForm.jsx';
 
 const Word = ({word}) => {
-  return <div>{word}</div>
-};
+  const [letter, setLetter] = useState('');
 
+  const handleSubmit = (e, letter) => {
+    setLetter(letter);
+  }
+
+  return (
+    <div>
+      {word}
+      <LetterGuessForm handleSubmit={handleSubmit} />
+    </div>
+  )
+};
+// build a string of '_' using the word length - put space in between each in display
+// check user input to see if the word contains the letter
+// change the string and put the character in place of the '_' anywhere it is
+// i.e. if a word has 2 like 'apple' and the person guesses the 'p', the string would be '_pp__'
 export default Word;
