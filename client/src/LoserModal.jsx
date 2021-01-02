e501562672f2f055277e38ba4cb6f045938b9fac
Deb Johnson <friendswithdrj@gmail.com>
2021-01-02 17:37:14 -0600

    Add buttons to both modals to quit/go to next word

--- client/src/LoserModal.jsx
+++ client/src/LoserModal.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function LoserModal() {
-  return <div>Better Luck Next Time!</div>
+  return (
+    <div className="modal">
+      <h2>Better Luck Next Time!</h2>
+      <button>Quit Game</button>
+      <button>Next Word</button>
+    </div>
+  )
 }
-
+// Quit Game => when clicked 'log out' the user
+// Next Word => when clicked change to next word from word array
 function Modal(props) {
   return ReactDOM.createPortal(LoserModal, document.querySelector("#loserModal"));
 }
--- client/src/WinnerModal.jsx
+++ client/src/WinnerModal.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function WinnerModal() {
-  return <div>WINNER!</div>
+  return (
+    <div className="modal">
+      <h2>WINNER!</h2>
+      <button>Quit Game</button>
+      <button>Next Word</button>
+    </div>
+  )
 }
-
+// Quit Game => when clicked 'log out' the user
+// Next Word => when clicked change to next word from word array
 
 function Modal(props) {
   return ReactDOM.createPortal(WinnerModal, document.querySelector("#winnerModal"));
