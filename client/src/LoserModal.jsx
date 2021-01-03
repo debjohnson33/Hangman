4230cd0c3269e3ab74b965ab98bb088566ceee27
Deb Johnson <friendswithdrj@gmail.com>
2021-01-02 20:04:28 -0600

    Add word as prop for LoserModal

--- client/src/LoserModal.jsx
+++ client/src/LoserModal.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './style.css';
 
-function LoserModal({onQuit, onNextWord}) {
+function LoserModal({onQuit, onNextWord, word}) {
   return (
     <div className={styles.modal}>
       <h2 className={styles.loser}>Better Luck Next Time!</h2>
+      <p>The word was: {word}</p>
       <button className={styles.btn} onClick={onQuit} >Quit Game</button>
       <button className={styles.btn} onClick={onNextWord} >Next Word</button>
     </div>
