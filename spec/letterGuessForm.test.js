95e2ae98daa3aa119a721f24ca222c1a19c582bd
Deb Johnson <friendswithdrj@gmail.com>
2021-01-04 17:05:17 -0600

    Add test for letterGuessForm component

---
+++ spec/letterGuessForm.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import LetterGuessForm from '../client/src/components/LetterGuessForm.jsx';
+
+Enzyme.configure({adapter: new Adapter() });
+
+describe('User Form Component', () => {
+  it('should render', () => {
+    const wrapper = shallow(<LetterGuessForm />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('should have a form', () => {
+    const wrapper = shallow(<LetterGuessForm />);
+    const h2 = wrapper.find('h2');
+    const form = wrapper.find('form');
+    expect(form.exists()).toBeTruthy();
+    expect(h2.text()).toBe('Type in One Letter:');
+  });
+});
